09cb9d9e452eba96bf3b7b44c659be47e9043a44
cuteHU <541707796@qq.com>
2020-03-18 16:29:12 +0800

    环境的设置

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
 </template>
 
 <script>
-
+import storage from './storage'
 export default {
   name: 'App',
   components: {
-  }
+  },
+  mounted () {
+    // storage.setItem('a', 1);
+    // storage.setItem('user', { a: 1 })
+    // storage.setItem('abc', { a: 1 }, 'user')
+    storage.clear('a', 'user');
+  },
 }
 </script>
 
 <style>
-
 </style>
